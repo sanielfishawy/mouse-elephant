4842250478075a8092a27c19c7bfa63eb158715b
Sani Elfishawy <elfishawy.sani@gmail.com>
2024-10-24 11:47:59 -0700

    min max

--- models/Client.js
+++ models/Client.js
@@ -94,7 +94,7 @@ export default class Client{
             numRequests: Stats.numRequests,
             totalSeconds,
             requestsPerSecond: Stats.numRequests / totalSeconds,
-            ...Stats.getStats(),
+            ...Stats.stats,
         })
     }
 
--- models/Stats.js
+++ models/Stats.js
@@ -3,10 +3,15 @@ export default class Stats{
     static l_2 = 0
     static b_2_5 = 0
     static g_5 = 0
+    static min = 100000
+    static max = 0
 
 
     static recordDelay(delay){
         this.numRequests++
+
+        if (delay < this.min) this.min = delay
+        if (delay > this.max) this.max = delay
 
         if (delay <= 2)
             this.l_2++
@@ -20,8 +25,10 @@ export default class Stats{
         return this.numRequests
     }
 
-    static getStats(){
+    static get stats(){
         return {
+            min: this.min,
+            max: this.max,
             l_2: this.l_2,
             b_2_5: this.b_2_5,
             g_5: this.g_5
