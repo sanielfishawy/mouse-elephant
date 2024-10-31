7439c0bfabd77d48f44dcf2cb09f39fccd126eb1
Sani Elfishawy <elfishawy.sani@gmail.com>
2024-10-30 19:12:49 -0700

    rtt

--- run/runClient.js
+++ run/runClient.js
@@ -2,4 +2,4 @@ import Client from '../models/Client.js'
 import Config from '../config/Config.js'
 
 const serverIp = process.argv[2] || Config.serverIp
-new Client({host: serverIp, port: 6000, size: Config.smallName, gapUs: 0, numRequests: 300000})
+new Client({host: serverIp, port: 6000, size: Config.smallName, gapUs: 0, numRequests: 10000000})
