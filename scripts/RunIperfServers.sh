b0c4f92c0e6e7953795fc983ce7ee7cdbb7622b8
Sani Elfishawy <elfishawy.sani@gmail.com>
2024-11-04 12:13:01 -0800

    client

--- scripts/RunIperfServers.sh
+++ scripts/RunIperfServers.sh
@@ -1,7 +1,7 @@
 taskset -c 2 iperf3 -s -p 5201 -D
-taskset -c 3 iperf3 -s -p 5202 -D
-taskset -c 4 iperf3 -s -p 5203 -D
+taskset -c 2 iperf3 -s -p 5202 -D
+taskset -c 2 iperf3 -s -p 5203 -D
 
-taskset -c 1 iperf3 -c 10.0.1.61 -p 5201 -t 0 -P 10
-taskset -c 1 iperf3 -c 10.0.1.61 -p 5202 -t 0 -P 10
-taskset -c 1 iperf3 -c 10.0.1.61 -p 5203 -t 0 -P 10
+taskset -c 1 iperf3 -c 10.0.1.45 -p 5201 -t 0 -P 10
+taskset -c 1 iperf3 -c 10.0.1.45 -p 5202 -t 0 -P 10
+taskset -c 1 iperf3 -c 10.0.1.45 -p 5203 -t 0 -P 10
